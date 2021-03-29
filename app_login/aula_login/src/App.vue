03acb4fd2fcb575dc7285fcf2d2fca4242382da8
guilhermesordi <silvadesordi@gmail.com>
2021-03-29 09:01:59 -0300

    diajwbduhbwa

--- app_login/aula_login/src/App.vue
+++ app_login/aula_login/src/App.vue
@@ -1,51 +1,102 @@
 <template>
     <div class="container">
-        <div id="login" v-if="logged == 0 && !createAccount">
-        <div class="title">Login</div>
-        <p />
-        <input class="button" v-model="login" placeholder="Login" />
-        <p />
-        <input class="button" v-model="senha" placeholder="Senha" />
-        <p />
-        <button v-on:click="logged = checkLogin(login, senha)">
-            Fazer Login
-        </button>
-        <p />
-        <button v-on:click="createAccount = true">Criar Conta</button>
+        <div id="login" v-if="pagina==1">
+            <div class="title">Login</div>
+            <p />
+            <input class="input" v-model="login" placeholder="Login" />
+            <p />
+            <input class="input" v-model="senha" placeholder="Senha" />
+            <p />
+            <button class="button" @click="verifyLogin()">
+                Fazer Login
+            </button>
+            <p />
+            <button class="button" @click="createAcc()">
+                Criar Conta
+            </button>
         </div>
-        <createAcc @newUsers="eventListener" v-if="createAccount"></createAcc>
-        <ai v-if="logged == 2"></ai>
-        <ui v-if="logged == 1"></ui>
-        <p />
-        <button @click="home()">Voltar</button>
+        <!-- Caso não esteje em Home: -->
+        <div v-if="pagina!=1">
+            <div class="title" id="telaAdmin" v-if="pagina==2">
+                Tela do Admin
+                
+            </div>
+            <div class="title" id="telaUser" v-if="pagina==3">
+                Tela do Usuário
+            </div>
+            <p />
+            <div class="title" id="telaCreate" v-if="pagina==4">
+                Criar Conta
+                <p />
+                <input class="input" v-model="login1" placeholder="Login" />
+                <p />
+                <input class="input" v-model="pass1" placeholder="Senha" />
+                <p />
+                <input class="input" v-model="pass2" placeholder="Repita a senha" />
+                <p />
+                <button class="button" @click="checkCreate()">Criar</button>
+            </div>
+            <p />
+            <button class="button" @click="home()">Home</button>
+        </div>
     </div>
 </template>
 
 <script>
-import ui from "./components/ui.vue";
-import checkLogin from "./components/telalogin";
-import eventListener from "./components/telalogin";
-import ai from "./components/ai.vue";
-import createAcc from "./components/createAcc";
-
 export default {
-  components: { ai, ui, createAcc },
+  components: {},
   data() {
     return {
         login: "",
         senha: "",
-        logged: "",
-        createAccount: false,
+        login1: "",
+        pass1: "",
+        pass2: "", 
+        pagina: 1,
+        users: [],
+        admin: "admin"
     };
   },
-    methods: {
-        checkLogin,
-        eventListener,
-        home() {
-            this.logged = 0;
-            this.createAccount = false;
-        },
+
+  methods: {
+    verifyLogin() {
+        
+        for(let i = 0; i < this.users.length; i++){
+            if(this.login == this.users[i].name && this.senha == this.users[i].pass){
+                this.pagina = 3;
+                this.login = "";
+                this.senha = "";
+                return
+            }
+        }
+        if(this.login == this.admin && this.senha == this.admin){
+            this.pagina = 2;
+            this.login = "";
+            this.senha = "";
+            return
+        }
+        alert("Crendenciais incorretas!")
+        
     },
+    home() {
+        this.pagina = 1;
+    },
+    createAcc(){
+        this.pagina = 4
+    },
+    checkCreate(){
+        if(this.login1 != "" && this.pass1 != "" && this.pass1 === this.pass2){
+            this.users.push({name: this.login1, pass: this.pass1})
+            alert("Conta criada com sucesso!")
+            console.log(this.users)
+        }else{
+            alert("Credenciais incorretas")
+        }
+        this.login1 = "";
+        this.pass1 = "";
+        this.pass2 = "";
+    }
+  },
 };
 </script>
 
@@ -68,7 +119,17 @@ body {
   font-weight: 600;
 }
 
+.input {
+  padding: 6px;
+  border-radius: 10px;
+  align-content: center;
+}
+
 .button {
+  font-family: "Roboto Bold", monospace;
+  font-weight: 800;
+  color: black;
+  background-color: white;
   padding: 6px;
   border-radius: 10px;
 }
